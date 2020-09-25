d4c99ff573f8bb54d378d2e15505514988711f33
Ricardo Olalquiaga <66755477+Icksir@users.noreply.github.com>
2020-09-24 22:02:22 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,8 +7,25 @@ $(function() {
     var url = api + buscado;
     $.getJSON(url, function(data) {
       currentPokemonId = data.id;
-      agregarTexto(data.name);
-      agregarFoto(data.sprites.front_default);
+      var tipo = $('#tipo-pokemon').val();
+      var elegido = $('#buscador').val();
+      agregarTexto(data.name);  
+      if (tipo == 'shiny'){
+        agregarFoto(data.sprites.front_shiny)
+      } else {
+        if (data.sprites.other['official-artwork'].front_default != null){
+          agregarFoto(data.sprites.other['official-artwork'].front_default);
+        }
+        else{
+          agregarFoto(data.sprites.front_default);
+        }
+      }
+
+      if (elegido > 0){
+        document.getElementById('buscador').value = currentPokemonId;
+      } else {
+        document.getElementById('buscador').value = data.name;
+      }
     })
   }
 
